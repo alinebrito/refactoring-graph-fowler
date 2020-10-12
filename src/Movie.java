4e1142b965ee890dda3482cacc2c6da8e3437e08
alinebrito <nbrito.aline@gmail.com>
2020-10-12 18:14:06 -0300

    Commit 10: Extract and move

--- src/Movie.java
+++ src/Movie.java
@@ -44,4 +44,13 @@ public class Movie {
 		}
 		return thisAmount;
 	}
+	
+	public int getFrequentRenterPoints(int daysRented) {
+		// add frequent renter points
+		// add bonus for a two day new release rental
+		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
+			return 2;
+		else
+			return 1;
+	}
 }
--- src/Rental.java
+++ src/Rental.java
@@ -21,11 +21,6 @@ public class Rental {
 	}
 	
 	public int getFrequentRenterPoints() {
-		// add frequent renter points
-		// add bonus for a two day new release rental
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-			return 2;
-		else
-			return 1;
+		return _movie.getFrequentRenterPoints(_daysRented);
 	}
 }
