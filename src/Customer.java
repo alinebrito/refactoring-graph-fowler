2b730d4135db32156d43fbae9617de5fcbb25a1b
alinebrito <nbrito.aline@gmail.com>
2020-10-12 18:06:56 -0300

    Commit 8: New feature

--- src/Customer.java
+++ src/Customer.java
@@ -36,6 +36,24 @@ public class Customer {
 		return result;
 	}
 	
+	public String htmlStatement() {
+		Enumeration rentals = _rentals.elements();
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			// show figures for each rental
+			result += each.getMovie().getTitle()+ ": " +
+			String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+
+		// add footer lines
+		result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+		result += "On this rental you earned <EM>" +
+		String.valueOf(getTotalFrequentRenterPoints()) +
+		"</EM> frequent renter points<P>";
+		return result;
+	}
+	
 	private double getTotalCharge() {
 		double result = 0;
 		Enumeration rentals = _rentals.elements();
