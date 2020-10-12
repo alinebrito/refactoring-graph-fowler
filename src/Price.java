1ca270264ee8635a331a694c6ba78486eb5e6414
alinebrito <nbrito.aline@gmail.com>
2020-10-12 18:24:28 -0300

    Commit 12: Extract and move method

--- src/Price.java
+++ src/Price.java
@@ -3,4 +3,25 @@ public abstract class Price {
 	
 	public abstract int getPriceCode();
 	
+	public double getCharge(int daysRented) {
+		double thisAmount = 0;
+		//determine amounts for each line
+		switch (getPriceCode()) {
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (daysRented > 2)
+					thisAmount += (daysRented - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += daysRented * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (daysRented > 3)
+				thisAmount += (daysRented - 3) * 1.5;
+				break;
+		}
+		return thisAmount;
+	}
+	
 }
