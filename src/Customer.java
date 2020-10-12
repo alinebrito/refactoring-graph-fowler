9f277d0d13a4e484b53d5d018bbeaed06aa50ff3
alinebrito <nbrito.aline@gmail.com>
2020-10-12 17:49:17 -0300

    Commit 3: Rename

--- src/Customer.java
+++ src/Customer.java
@@ -49,22 +49,22 @@ public class Customer {
 		return result;
 	}
 
-	private double amountFor(Rental each) {
+	private double amountFor(Rental aRental) {
 		double thisAmount = 0;
 		//determine amounts for each line
-		switch (each.getMovie().getPriceCode()) {
+		switch (aRental.getMovie().getPriceCode()) {
 			case Movie.REGULAR:
 				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				if (aRental.getDaysRented() > 2)
+					thisAmount += (aRental.getDaysRented() - 2) * 1.5;
 				break;
 			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
+				thisAmount += aRental.getDaysRented() * 3;
 				break;
 			case Movie.CHILDRENS:
 				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-				thisAmount += (each.getDaysRented() - 3) * 1.5;
+				if (aRental.getDaysRented() > 3)
+				thisAmount += (aRental.getDaysRented() - 3) * 1.5;
 				break;
 		}
 		return thisAmount;
