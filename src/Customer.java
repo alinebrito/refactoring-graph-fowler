0a99156638e534c939163cf1f0afae87f27f4599
alinebrito <nbrito.aline@gmail.com>
2020-10-12 17:54:23 -0300

    Commit 5: Replace temp with query

--- src/Customer.java
+++ src/Customer.java
@@ -25,10 +25,7 @@ public class Customer {
 		Enumeration rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
-			
-			thisAmount = each.getCharge();
 			
 			// add frequent renter points
 			frequentRenterPoints ++;
@@ -38,8 +35,8 @@ public class Customer {
 			
 			//show figures for this rental
 			result += "\t" + each.getMovie().getTitle()+ "\t" +
-			String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
 		
 		}
 		//add footer lines
