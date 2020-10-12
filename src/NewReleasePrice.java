b6490a7508345983d2708537c262247a6122c58e
alinebrito <nbrito.aline@gmail.com>
2020-10-12 18:33:06 -0300

    Commit 15: Inheritance

--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -8,4 +8,8 @@ public class NewReleasePrice extends Price {
 	public double getCharge(int daysRented){
 		return daysRented * 3;
 	} 
+	
+	public int getFrequentRenterPoints(int daysRented) {
+		return (daysRented > 1) ? 2: 1;
+	}
 }
--- src/Price.java
+++ src/Price.java
@@ -6,12 +6,7 @@ public abstract class Price {
 	public abstract double getCharge(int daysRented);
 	
 	public int getFrequentRenterPoints(int daysRented) {
-		// add frequent renter points
-		// add bonus for a two day new release rental
-		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-			return 2;
-		else
-			return 1;
+		return 1;
 	}
 	
 }
